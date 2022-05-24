991dbd2f6a94a68860d8530e9f2ce13519845da2
天玎 <tianding.wk@antgroup.com>
2022-05-24 10:25:02 +0800

    test: add coverage

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,9 @@
 
 const assert = require('assert');
 const nock = require('nock');
+const fs = require('fs').promises;
+const os = require('os');
+const path = require('path');
 const { MirrorConfig, mirrors } = require('..');
 
 describe('test/index.test.js', () => {
@@ -69,6 +72,73 @@ describe('test/index.test.js', () => {
         },
       });
       assert(nockScope.isDone());
+    });
+  });
+
+  describe('method test', () => {
+    let root;
+    beforeEach(async () => {
+      root = path.join(os.tmpdir(), Date.now() + '');
+      fs.mkdir(root, {
+        recursive: true,
+      });
+    });
+
+    afterEach(async () => {
+      console.info('rot: ', root);
+      await fs.rmdir(root, {
+        force: true,
+        recursive: true,
+      });
+    });
+    it('should work', async () => {
+      const mirrorConfig = new MirrorConfig({});
+      await mirrorConfig.init();
+      const options = {
+        env: {
+        },
+      };
+
+      const pkg = {
+        name: 'canvas',
+        binary: {
+          host: 'https://www.alipay.com',
+        },
+      };
+
+      mirrorConfig.setEnvs(options);
+      assert.deepStrictEqual(options, {
+        env: {
+          NODEJS_ORG_MIRROR: 'https://cdn.npmmirror.com/binaries/node',
+          NVM_NODEJS_ORG_MIRROR: 'https://cdn.npmmirror.com/binaries/node',
+          PHANTOMJS_CDNURL: 'https://cdn.npmmirror.com/binaries/phantomjs',
+          CHROMEDRIVER_CDNURL: 'https://cdn.npmmirror.com/binaries/chromedriver',
+          OPERADRIVER_CDNURL: 'https://cdn.npmmirror.com/binaries/operadriver',
+          ELECTRON_MIRROR: 'https://cdn.npmmirror.com/binaries/electron/',
+          ELECTRON_BUILDER_BINARIES_MIRROR: 'https://cdn.npmmirror.com/binaries/electron-builder-binaries/',
+          SASS_BINARY_SITE: 'https://cdn.npmmirror.com/binaries/node-sass',
+          SWC_BINARY_SITE: 'https://cdn.npmmirror.com/binaries/node-swc',
+          NWJS_URLBASE: 'https://cdn.npmmirror.com/binaries/nwjs/v',
+          PUPPETEER_DOWNLOAD_HOST: 'https://cdn.npmmirror.com/binaries',
+          SENTRYCLI_CDNURL: 'https://cdn.npmmirror.com/binaries/sentry-cli',
+          SAUCECTL_INSTALL_BINARY_MIRROR: 'https://cdn.npmmirror.com/binaries/saucectl',
+          RE2_DOWNLOAD_MIRROR: 'https://cdn.npmmirror.com/binaries/node-re2',
+          RE2_DOWNLOAD_SKIP_PATH: 'true',
+          npm_config_keytar_binary_host: 'https://cdn.npmmirror.com/binaries/keytar',
+          npm_config_sharp_binary_host: 'https://cdn.npmmirror.com/binaries/sharp',
+          npm_config_sharp_libvips_binary_host: 'https://cdn.npmmirror.com/binaries/sharp-libvips',
+          npm_config_robotjs_binary_host: 'https://cdn.npmmirror.com/binaries/robotjs',
+        },
+      });
+
+      await mirrorConfig.updatePkg(root, pkg);
+      const latestPkg = await fs.readFile(path.join(root, 'package.json'), 'utf8');
+      assert.deepStrictEqual(JSON.parse(latestPkg), {
+        name: 'canvas',
+        binary: {
+          host: 'https://cdn.npmmirror.com/binaries/canvas',
+        },
+      });
     });
   });
 });
