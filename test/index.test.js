1335e8ae199f8a1df3d10b5d3217094a40bb8b3d
elrrrrrrr <elrrrrrrr@gmail.com>
2023-05-05 13:32:40 +0800

    feat: add puppeteer new env

--- test/index.test.js
+++ test/index.test.js
@@ -301,6 +301,7 @@ describe('test/index.test.js', () => {
           SWC_BINARY_SITE: 'https://cdn.npmmirror.com/binaries/node-swc',
           NWJS_URLBASE: 'https://cdn.npmmirror.com/binaries/nwjs/v',
           PUPPETEER_DOWNLOAD_HOST: 'https://cdn.npmmirror.com/binaries',
+          PUPPETEER_DOWNLOAD_BASE_URL: 'https://cdn.npmmirror.com/binaries/chrome-for-testing',
           SENTRYCLI_CDNURL: 'https://cdn.npmmirror.com/binaries/sentry-cli',
           SAUCECTL_INSTALL_BINARY_MIRROR: 'https://cdn.npmmirror.com/binaries/saucectl',
           RE2_DOWNLOAD_MIRROR: 'https://cdn.npmmirror.com/binaries/node-re2',
